ac39bd320c489b44f0d7b46c69daa81ea8d1313b
Robert Lowe <robertmlowe@rmlowe.com>
2014-12-10 23:15:39 +0800

    Version 1.1: support for optional 'limit' argument to limit number of reports downloaded

--- batch-report-downloader/README.md
+++ batch-report-downloader/README.md
@@ -2,8 +2,16 @@
 
 Simple command-line application that downloads a user's batch reports to a local directory.
 
-## Sample usage
+## Usage
+
+The Batch Report Downloader is invoked as shown below. The optional `<limit>` argument specifies the maximum number of reports to download, which will be the most recent. 
 
 ```shell
-java -jar batch-report-downloader-1.0-jar-with-dependencies.jar https://www.example.com/ekp/ johndoe mypassword .
+java -jar batch-report-downloader-1.1-jar-with-dependencies.jar <Talent Suite base URL> <user ID> <password> <target directory> <limit>
+```
+
+For example, the command below downloads at most the three most recent reports for user **johndoe** from the site at `https://www.example.com/ekp/`.
+
+```shell
+java -jar batch-report-downloader-1.1-jar-with-dependencies.jar https://www.example.com/ekp/ johndoe mypassword . 3
 ```
--- batch-report-downloader/src/main/java/com/netdimensions/client/reports/BatchReportDownloader.java
+++ batch-report-downloader/src/main/java/com/netdimensions/client/reports/BatchReportDownloader.java
@@ -3,6 +3,8 @@ package com.netdimensions.client.reports;
 import java.io.File;
 import java.io.IOException;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Iterables;
 import com.google.common.io.Files;
 import com.netdimensions.client.Client;
 import com.netdimensions.client.Credentials;
@@ -12,7 +14,8 @@ import com.netdimensions.client.Request;
 public final class BatchReportDownloader {
 	public static void main(final String[] args) throws IOException {
 		final Client client = new Client(args[0], Credentials.basic(args[1], args[2]));
-		for (final Report report : client.send(Request.batchReports())) {
+		final ImmutableList<Report> reports = client.send(Request.batchReports());
+		for (final Report report : args.length > 4 ? Iterables.limit(reports, Integer.parseInt(args[4])) : reports) {
 			final String pathname = report.pathname();
 			Files.asByteSink(new File(args[3], pathname.substring(pathname.lastIndexOf('\\') + 1))).write(client.send(Request.batchReport(pathname)));
 		}
